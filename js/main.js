2a4845890b42418156ef07d3277fc9f0f9fd95e3
Rarce95 <rogarce15@gmail.com>
2020-07-11 01:35:33 -0600

    nav cambios

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const navar = ()=>{
+    var ck = document.querySelector('.checkbtn');
+    var nav = document.querySelector('.nav__list');
+    ck.addEventListener('click',()=>{
+        nav.style.left=0;
+    })
+}
