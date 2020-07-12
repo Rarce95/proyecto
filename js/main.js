066b4d4d0477a1c8f8db63073bee5713bb0a9dfc
Rarce95 <rogarce15@gmail.com>
2020-07-12 06:23:22 -0600

    colors

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-const navar = ()=>{
-    var ck = document.querySelector('.checkbtn');
-    var nav = document.querySelector('.nav__list');
-    ck.addEventListener('click',()=>{
-        nav.style.left=0;
-    })
+
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('.navbar');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+const colorSwitch = document.querySelector('theme-switch input[type="checkbox]');
+
+function switchColor(e){
+    if(e.target.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+    }else{
+        document.documentElement.setAttribute('data-theme','light');
+    }
 }
+
+colorSwitch.addEventListener('change',switchColor,false);
